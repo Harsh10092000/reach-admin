45a253e706476f27fa3b7a57bea885eb704a9316
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-03-08 16:51:06 +0530

    add nice admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-//import './App.css'
+import './App.css'
 
 import AdminPanel from "./AdminPanel"
 import FormComponent from "./FormComponent"
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
      <div>test</div>
-     <AdminPanel />
+     <FormComponent />
     </>
   )
 }
